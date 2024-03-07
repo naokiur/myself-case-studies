24fb63acca86bfe212e08ced0b41a5332df91129
naokiur <atomic.transfer.comp@gmail.com>
2024-03-07 22:17:26 +0900

    feat(test): 1行目と2行目が片方のみ、1行目と2行目が両方複数の場合のテスト追加. 0よりの境界値と考えたため.

--- sample-java-fictional-shop-checkout/src/test/java/org/example/controller/ReturnChangeParamTest.java
+++ sample-java-fictional-shop-checkout/src/test/java/org/example/controller/ReturnChangeParamTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class ReturnChangeParamTest {
 
   @Test
-  void パラメータ生成確認_1行目のが1つ_2行目が1つ_3行目が1つである場合クラスを生成できること() {
+  void パラメータ生成確認_1行目が1つ_2行目が1つ_3行目が1つである場合クラスを生成できること() {
     var firstParam = "001";
     var secondParam = "831:100";
     var thirdParam = "200";
@@ -20,18 +20,50 @@ class ReturnChangeParamTest {
     assertEquals("100", target.directItems.get("831"));
     assertEquals("200", target.money);
   }
+
   @Test
-  void パラメータ生成確認_1行目のが2つ_2行目が1つ_3行目が1つである場合クラスを生成できること() {
+  void パラメータ生成確認_1行目が2つ_2行目が0_3行目が1つである場合クラスを生成できること() {
     var firstParam = "001,002";
-    var secondParam = "831:200";
+    var secondParam = "";
+    var thirdParam = "200";
+    var target = new ReturnChangeParam(firstParam, secondParam, thirdParam);
+
+    assertEquals(List.of("001", "002"), target.codes);
+    assertEquals(0, target.directItems.size());
+    assertEquals("200", target.money);
+  }
+
+  @Test
+  void パラメータ生成確認_1行目が0_2行目が2つ_3行目が1つである場合クラスを生成できること() {
+    var firstParam = "";
+    var secondParam = "831:200,220:300";
+    var thirdParam = "200";
+    var target = new ReturnChangeParam(firstParam, secondParam, thirdParam);
+
+    assertEquals(0, target.codes.size());
+    assertTrue(target.directItems.containsKey("831"));
+    assertTrue(target.directItems.containsKey("220"));
+    assertEquals("200", target.directItems.get("831"));
+    assertEquals("300", target.directItems.get("220"));
+    assertEquals("200", target.money);
+  }
+
+
+  @Test
+  void パラメータ生成確認_1行目が2_2行目が2つ_3行目が1つである場合クラスを生成できること() {
+    var firstParam = "001,002";
+    var secondParam = "831:200,220:300";
     var thirdParam = "200";
     var target = new ReturnChangeParam(firstParam, secondParam, thirdParam);
 
     assertEquals(List.of("001", "002"), target.codes);
     assertTrue(target.directItems.containsKey("831"));
+    assertTrue(target.directItems.containsKey("220"));
     assertEquals("200", target.directItems.get("831"));
+    assertEquals("300", target.directItems.get("220"));
     assertEquals("200", target.money);
   }
+
   @Test
   void 期待と異なる形式がパラメータの場合も生成可能ではあること() {
     var firstParam = "001,002,,,test,,";
