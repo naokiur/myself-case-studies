f257225c4826e0a5a4a0b1bf4779745d6385a814
naokiur <atomic.transfer.comp@gmail.com>
2024-03-15 22:36:17 +0900

    feat(controller): パラメータ例外を追加（識別番号の個数、種別番号） システム動作環境を超過することを防ぐため、早いレイヤー（controller）で対応.

--- fictional-shop-checkout/example-java/src/main/java/org/example/controller/ReturnChangeParam.java
+++ fictional-shop-checkout/example-java/src/main/java/org/example/controller/ReturnChangeParam.java
@@ -24,11 +24,32 @@ class ReturnChangeParam {
             v -> v.length() <= 100
         ));
     if (!codesMap.get(false).isEmpty()) {
-      throw new ParametersException("識別番号が100文字を超えている商品が含まれています。");
+      throw new ParametersException("1行目のパラメータ（識別番号）に100文字を超えた文字列が含まれているため、扱うことができません。");
+    }
+
+    if (codesMap.get(true).size() > 50) {
+      throw new ParametersException("1行目のパラメータ（識別番号）の個数が50個を超えているため、扱うことができません。");
     }
     this.codes = codesMap.get(true);
 
     // ブランクができた場合、または":"が含まれない場合は除去する
+    var directItemsMap = Stream.of(secondParam.split(PARAM_ROW_SEPARATE))
+        .filter(v -> !v.isEmpty())
+        .filter(v -> v.contains(PARAM_KEY_VALUE_SEPARATE)).collect(
+            Collectors.partitioningBy(
+                // xxx:xxxという形式のため、0文字から:までを種別番号として扱い、その文字数をチェックする
+                v -> v.substring(0, v.indexOf(PARAM_KEY_VALUE_SEPARATE)).length() <= 100
+            )
+        );
+
+    if (!directItemsMap.get(false).isEmpty()) {
+      throw new ParametersException("2行目のパラメータ（種別番号:値段）の1つ目（種別番号）に100文字を超えた文字列が含まれてるため、扱うことができません。");
+    }
+
+    if (directItemsMap.get(true).size() > 50) {
+      throw new ParametersException("2行目のパラメータ（種別番号:値段）の個数が50個を超えているため、扱うことができません。");
+    }
+
     this.directItems = Stream.of(secondParam.split(PARAM_ROW_SEPARATE))
         .filter(v -> !v.isEmpty())
         .filter(v -> v.contains(PARAM_KEY_VALUE_SEPARATE)).toList();
--- fictional-shop-checkout/example-java/src/test/java/org/example/controller/ReturnChangeParamTest.java
+++ fictional-shop-checkout/example-java/src/test/java/org/example/controller/ReturnChangeParamTest.java
@@ -70,11 +70,42 @@ class ReturnChangeParamTest {
   }
 
   @Test
-  void パラメータ異常_1行目の1つの文字数が100を超えるときパラメータ例外を発生させること() {
+  void パラメータ異常_1行目の1つの文字数が100文字を超えるときパラメータ例外を発生させること() {
     var firstParam = "01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567891";
     var secondParam = "831:200";
     var thirdParam = "200";
 
-    assertThrows(ParametersException.class, () -> new ReturnChangeParam(firstParam, secondParam, thirdParam));
+    ParametersException e = assertThrows(ParametersException.class, () -> new ReturnChangeParam(firstParam, secondParam, thirdParam));
+    assertEquals("1行目のパラメータ（識別番号）に100文字を超えた文字列が含まれているため、扱うことができません。", e.getMessage());
+  }
+
+  @Test
+  void パラメータ異常_1行目の個数が50個を超えるときパラメータ例外を発生させること() {
+    var firstParam = "001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001,001";
+    var secondParam = "831:200";
+    var thirdParam = "200";
+
+    ParametersException e = assertThrows(ParametersException.class, () -> new ReturnChangeParam(firstParam, secondParam, thirdParam));
+    assertEquals("1行目のパラメータ（識別番号）の個数が50個を超えているため、扱うことができません。", e.getMessage());
+  }
+
+  @Test
+  void パラメータ異常_2行目の1つ目の文字列_種別番号が100文字を超えるときパラメータ例外を発生させること() {
+    var firstParam = "";
+    var secondParam = "01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567891:200";
+    var thirdParam = "200";
+
+    ParametersException e = assertThrows(ParametersException.class, () -> new ReturnChangeParam(firstParam, secondParam, thirdParam));
+    assertEquals("2行目のパラメータ（種別番号:値段）の1つ目（種別番号）に100文字を超えた文字列が含まれてるため、扱うことができません。", e.getMessage());
+  }
+
+  @Test
+  void パラメータ異常_2行目の個数が50個を超えるときパラメータ例外を発生させること() {
+    var firstParam = "";
+    var secondParam = "831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200,831:200";
+    var thirdParam = "200";
+
+    ParametersException e = assertThrows(ParametersException.class, () -> new ReturnChangeParam(firstParam, secondParam, thirdParam));
+    assertEquals("2行目のパラメータ（種別番号:値段）の個数が50個を超えているため、扱うことができません。", e.getMessage());
   }
 }
