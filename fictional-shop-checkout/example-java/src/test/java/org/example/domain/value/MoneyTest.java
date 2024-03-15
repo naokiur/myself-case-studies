cb17fdc6eccf6db8177e962526d065b422c7b469
naokiur <atomic.transfer.comp@gmail.com>
2024-03-15 22:59:23 +0900

    feat(domain): 支払い情報：金額のテストケースと処理を追加.

---
+++ fictional-shop-checkout/example-java/src/test/java/org/example/domain/value/MoneyTest.java
@@ -0,0 +1,25 @@
+package org.example.domain.value;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.example.domain.DomainException;
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+
+  @Test
+  void 数字でないとき例外が発生すること() {
+    var param = "abc";
+
+    assertThrows(NumberFormatException.class, () -> new Money(param));
+  }
+
+  @Test
+  void 値が1000000を超えるとき例外が発生すること() {
+    var param = "1000001";
+
+    DomainException e = assertThrows(DomainException.class, () -> new Money(param));
+    assertEquals("支払い情報：金額が1,000,000円を超えているため、扱うことができません。", e.getMessage());
+  }
+}
